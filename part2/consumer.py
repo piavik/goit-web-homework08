c76b4d8f0adf35d9a0e00b146ebd1636d4d055ba
piavik <piavik@gmail.com>
2024-01-21 20:45:29 +0100

    consumer.py is back

---
+++ part2/consumer.py
@@ -0,0 +1,28 @@
+import sys
+from models import Contact
+import connect_mongo
+import connect_rmq as rmq
+
+
+queue_name = "sms"
+
+def callback(ch, method, properties, body):
+    user_id = body.decode()
+    print(f'Message sent to user_id {user_id}')
+    db_contact = Contact.objects(id=user_id) 
+    db_contact.update(sent=True)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def main():
+    rmq.channel.queue_declare(queue=queue_name)
+    rmq.channel.basic_qos(prefetch_count=1)
+    rmq.channel.basic_consume(queue=queue_name, on_message_callback=callback)
+    rmq.channel.start_consuming()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        sys.exit(0)
